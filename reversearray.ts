fa9bf2c5803f93bc7ad3b86403068a0e7c1077ea
Wellington Silva <Wellingtontp0@gmail.com>
2022-09-10 22:33:49 -0300

    :octocat:

--- reversearray.ts
+++
@@ -1,3 +0,0 @@
-export const digitize = (n: number): number[] => {
-    return [...n.toString()].map(Number).reverse();
-  };
--- simpcondition.ts
+++
@@ -1,3 +0,0 @@
-export function even_or_odd(n:number):String {
-    return n % 2 == 0 ? "Even" : "Odd";
-  }
